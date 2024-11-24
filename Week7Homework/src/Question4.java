4d9f9c8bfbd3443b3ed26dfb762c7ab0e64d2359
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-24 19:29:07 +0000

    First push

---
+++ Week7Homework/src/Question4.java
@@ -0,0 +1,20 @@
+/*4. Write if else condition and print your group name in console else others group name.*/
+public class Question4 {
+
+    public static void main(String[] args) {
+        getResults(80);
+
+    }
+
+    public static void getResults(int i){
+
+        if (i>35) {
+
+            System.out.println("python");
+        }else{
+            System.out.println("java");
+        }
+
+
+    }
+}
